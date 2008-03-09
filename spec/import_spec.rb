696fa5564830d635ddb29ed4ac65f77b8372ea37
Greg Weber <greg@grubtop.(none)>
2008-03-09 10:03:38 -0500

    tested importing into modules

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -1,5 +1,33 @@
 require File.dirname(__FILE__) + '/../lib/import'
 
+def module_with_new
+  Module.new do
+    def self.new *args
+      (Class.new.send :include, self, *args).new
+    end
+  end
+end
+
+def class_with_super(superclass)
+  if superclass.is_a? Class
+    Class.new(superclass)
+  else
+    module_with_new().module_eval do
+      include superclass
+    end
+  end
+end
+
+def class_and_module(times=nil)
+  if !times or times == 1
+    [Class.new,  module_with_new()]
+  else
+    [(0...times).map{|i| Class.new}, (0...times).map{|i| module_with_new()}]
+  end
+end
+
+
+# testing uses this module
 module Foo
   def extra_method; fail end
 
@@ -11,93 +39,98 @@ end
 
 describe "import" do
   it 'should import only the required methods' do
-    klass = Class.new
-    klass.send :import, Foo, :bar
-    o = klass.new
-    o.bar.should == 'bar'
-    lambda{o.foo}.should raise_error(NoMethodError)
+    class_and_module.each do |k|
+      k.send :import, Foo, :bar
+      o = k.new
+      o.bar.should == 'bar'
+      lambda{o.foo}.should raise_error(NoMethodError)
+    end
   end
 
   it 'should raise an error when importing a method that does not exist' do
-    lambda{ Class.new.send :import, Foo, :foo, :not_defined, :bar }.
-      should raise_error(ImportError, /#not_defined/)
-    lambda{ Class.new.send :import, Foo,
+    class_and_module.each {|k| lambda{ k.send :import, Foo, :foo, :not_defined, :bar }.
+      should raise_error(ImportError, /#not_defined/) }
+
+    class_and_module.each {|k| lambda{ k.send :import, Foo,
       :not_defined, :foo, :_not_defined_, :bar, :__not_defined__ }.
-      should raise_error(ImportError, /#not_defined and #_not_defined_ and #__not_defined__/)
+      should raise_error(ImportError, /#not_defined and #_not_defined_ and #__not_defined__/) }
   end
 
   it 'should import all methods if none are given' do
-    c = Class.new
-    c.send :import, Foo
-    c.new.foo.should == 'foo'
-    c.new.bar.should == 'bar'
+    class_and_module.each do |c|
+      c.send :import, Foo
+      c.new.foo.should == 'foo'
+      c.new.bar.should == 'bar'
 
-    b = Class.new
-    b.send :import, Foo, *(Foo.private_instance_methods)
-    c.instance_methods.sort.should == b.instance_methods.sort
+      b = c.class.new
+      b.send :import, Foo, *(Foo.private_instance_methods)
+      c.instance_methods.sort.should == b.instance_methods.sort
 
-    a = Class.new
-    a.send :include, Foo
-    a.instance_methods.sort.should == c.instance_methods.sort
+      a = c.class.new
+      a.send :include, Foo
+      a.instance_methods.sort.should == c.instance_methods.sort
+    end
   end
 
   it 'should not be effected by changes to the module' do
-    module Bar
-      def foo; 'foo' end
+    class_and_module(2).each do |imp,inc|
+      Bar = Module.new do
+        def foo; 'foo' end
+      end
+      bar_orig_ims = Bar.instance_methods
+
+      imp.send :import, Bar
+      inc.send :include, Bar
+
+      imp.instance_methods.sort.should == inc.instance_methods.sort
+      (bar_orig_ims - imp.instance_methods).should be_empty
+
+      [imp, inc].each do |klass|
+        k = klass.new
+        k.foo.should == 'foo'
+        lambda{k.bar}.should raise_error(NoMethodError)
+      end
+
+      Bar.module_eval do
+        def extra_method; fail end
+
+        def bar; 'bar' end
+        undef_method :foo
+
+        def another_extra_method; fail end
+      end
+
+      lambda{inc.new.foo}.should raise_error(NoMethodError)
+      inc.new.bar.should == 'bar'
+
+      imp.new.foo.should == 'foo'
+      lambda{imp.new.bar}.should raise_error(NoMethodError)
+
+      (imp.instance_methods - inc.instance_methods).should == ['foo']
+      (inc.instance_methods - imp.instance_methods).sort.should == (Bar.instance_methods - bar_orig_ims).sort
     end
-    bar_orig_ims = Bar.instance_methods
-
-    imp = Class.new
-    imp.send :import, Bar
-    inc = Class.new
-    inc.send :include, Bar
-
-    [imp, inc].each do |klass|
-      k = klass.new
-      k.foo.should == 'foo'
-      lambda{k.bar}.should raise_error(NoMethodError)
-    end
-
-    imp.instance_methods.sort.should == inc.instance_methods.sort
-    (bar_orig_ims - imp.instance_methods).should be_empty
-
-    module Bar
-      def extra_method; fail end
-
-      def bar; 'bar' end
-      undef_method :foo
-
-      def another_extra_method; fail end
-    end
-
-    lambda{inc.new.foo}.should raise_error(NoMethodError)
-    inc.new.bar.should == 'bar'
-
-    imp.new.foo.should == 'foo'
-    lambda{imp.new.bar}.should raise_error(NoMethodError)
-
-    (imp.instance_methods - inc.instance_methods).should == ['foo']
-    (inc.instance_methods - imp.instance_methods).sort.should == (Bar.instance_methods - bar_orig_ims).sort
   end
 
   it 'should not effect calls to super' do
-    class A
-      def foo; 'super' end
+    class_and_module.each do |a|
+      a.class_eval do
+        def foo; 'super' end
+      end
+      b = class_with_super(a)
+      b.new.foo.should == 'super'
+
+      b.class_eval { import Foo, :bar }
+      b.new.foo.should == 'super'
+
+      bo = b.new
+      class << bo
+        import(Foo, :foo)
+      end
+      bo.foo.should == 'foo'
+      b.new.foo.should == 'super'
+
+      b.class_eval { import Foo, :foo }
+      b.new.foo.should == 'foo'
     end
-    class B < A; end
-    B.new.foo.should == 'super'
-
-    class B; import Foo, :bar end
-    B.new.foo.should == 'super'
-
-    b = B.new
-    class << b
-      import(Foo, :foo)
-    end
-    b.foo.should == 'foo'
-    B.new.foo.should == 'super'
-
-    class B; import Foo, :foo end
-    B.new.foo.should == 'foo'
   end
 end
