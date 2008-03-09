f5e34b7356028d93f77a0133a27ee9938813d953
Greg Weber <greg@grubtop.(none)>
2008-03-08 21:58:29 -0600

    revised

--- lib/import.rb
+++ lib/import.rb
@@ -1,54 +1,29 @@
 class ImportError < Exception; end
 
 module Kernel
-  # include an anonymous module with all uneeded instance methods removed
+
+  # include a duplicate of the module with all uneeded instance methods removed
   def import(mod, *meths)
-    include_anonymous_module = lambda do |block|
-      include( Module.new do
-        this = self
-
-        mod.module_eval do
-          (@@undef_methods_for ||= []).push(this)
-
-          class << self
-            unless self.respond_to? :__import_method_added__
-              alias_method :__import_method_added__, :method_added
-            end
-
-            def method_added(traced)
-              @@undef_methods_for.each do |mod|
-                mod.send :undef_method, traced #if mod.instance_methods.include? traced
-              end
-              self.send :__import_method_added__, traced
-            end
-          end
-
-        end
-
-        include mod
-        instance_eval &block if block
-      end )
+    include_module_copy = lambda do |block|
+      mod_dup = mod.dup
+      mod_dup.module_eval &block if block
+      include mod_dup
     end
 
     if meths.size == 0
-      include_anonymous_module.call(nil)
+      include_module_copy.call(nil)
 
     else
-      include_anonymous_module.call( lambda do
-
-        # get list of methods to remove from anonymous module
-        ims = mod.instance_methods.map{|m| m.to_sym}
-        meths.map do |meth|
-          ims.index(meth) || (raise ImportError, "##{meth} not found in #{mod}")
-        end.sort.reverse_each do |i|
-          ims.delete_at(i)
-        end
-
-        ims.each do |meth|
-          undef_method meth # breaks inheritance for the method
-        end
+      # get list of methods to remove module
+      ims = mod.instance_methods.map {|m| m.to_sym}
+      if (ims & meths).size != meths.size
+        raise ImportError, "##{(meths - ims).join(' and #')} not found in #{mod}"
       end
-      )
+      ims = ims - meths
+
+      include_module_copy.call( lambda do
+        ims.each { |meth| remove_method meth }
+      end )
     end
   end
 end
--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -1,21 +1,29 @@
 require File.dirname(__FILE__) + '/../lib/import'
 
 module Foo
+  def extra_method; fail end
+
   def foo; 'foo' end
   def bar; 'bar' end
+
+  def another_extra_method; fail end
 end
 
 describe "import" do
   it 'should import only the required methods' do
-    c = Class.new
-    c.send :import, Foo, :bar
-    c.new.bar.should == 'bar'
-    lambda{c.new.foo}.should raise_error(NoMethodError)
+    klass = Class.new
+    klass.send :import, Foo, :bar
+    o = klass.new
+    o.bar.should == 'bar'
+    lambda{o.foo}.should raise_error(NoMethodError)
   end
 
   it 'should raise an error when importing a method that does not exist' do
-    lambda{ Class.new.send :import, Foo, :not_defined }.
-      should raise_error(ImportError)
+    lambda{ Class.new.send :import, Foo, :foo, :not_defined, :bar }.
+      should raise_error(ImportError, /#not_defined/)
+    lambda{ Class.new.send :import, Foo,
+      :not_defined, :foo, :_not_defined_, :bar, :__not_defined__ }.
+      should raise_error(ImportError, /#not_defined and #_not_defined_ and #__not_defined__/)
   end
 
   it 'should import all methods if none are given' do
@@ -25,7 +33,7 @@ describe "import" do
     c.new.bar.should == 'bar'
 
     b = Class.new
-    b.send :import, Foo, :foo, :bar
+    b.send :import, Foo, *(Foo.private_instance_methods)
     c.instance_methods.sort.should == b.instance_methods.sort
 
     a = Class.new
@@ -37,31 +45,39 @@ describe "import" do
     module Bar
       def foo; 'foo' end
     end
+    bar_orig_ims = Bar.instance_methods
 
     imp = Class.new
     imp.send :import, Bar
-    imp.new.foo.should == 'foo'
-    lambda{imp.new.bar}.should raise_error(NoMethodError)
-
     inc = Class.new
     inc.send :include, Bar
-    inc.new.foo.should == 'foo'
-    lambda{inc.new.bar}.should raise_error(NoMethodError)
 
-    imp.instance_methods.should == inc.instance_methods
-
-    module Bar
-      def bar; 'bar' end
-      def foobar; 'foobar' end
+    [imp, inc].each do |klass|
+      k = klass.new
+      k.foo.should == 'foo'
+      lambda{k.bar}.should raise_error(NoMethodError)
     end
 
-    inc.new.foo.should == 'foo'
+    imp.instance_methods.sort.should == inc.instance_methods.sort
+    (bar_orig_ims - imp.instance_methods).should be_empty
+
+    module Bar
+      def extra_method; fail end
+
+      def bar; 'bar' end
+      undef_method :foo
+
+      def another_extra_method; fail end
+    end
+
+    lambda{inc.new.foo}.should raise_error(NoMethodError)
     inc.new.bar.should == 'bar'
 
     imp.new.foo.should == 'foo'
     lambda{imp.new.bar}.should raise_error(NoMethodError)
 
-    imp.instance_methods.should_not == inc.instance_methods
+    (imp.instance_methods - inc.instance_methods).should == ['foo']
+    (inc.instance_methods - imp.instance_methods).sort.should == (Bar.instance_methods - bar_orig_ims).sort
   end
 
   it 'should not effect calls to super' do
@@ -71,9 +87,17 @@ describe "import" do
     class B < A; end
     B.new.foo.should == 'super'
 
-    class B;
-      import Foo, :bar
-    end
+    class B; import Foo, :bar end
     B.new.foo.should == 'super'
+
+    b = B.new
+    class << b
+      import(Foo, :foo)
+    end
+    b.foo.should == 'foo'
+    B.new.foo.should == 'super'
+
+    class B; import Foo, :foo end
+    B.new.foo.should == 'foo'
   end
 end
