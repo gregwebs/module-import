df326359fc9c39b377b2c9dc23ccdf87359347d8
Greg Weber <greg@grubtop.(none)>
2008-03-11 13:44:20 -0500

    updated

--- lib/import.rb
+++ lib/import.rb
@@ -5,26 +5,23 @@ module Kernel
   # abstraction:: only include the methods given by _meths_
   # implementation:: includes a duplicate of _mod_ with all uneeded instance methods removed
   def import(mod, *meths)
-    include_module_copy = lambda do |block|
-      mod_dup = mod.dup
-      mod_dup.module_eval(&block) if block
-      include mod_dup
-    end
+    mod_dup = mod.dup
 
-    if meths.size == 0
-      include_module_copy.call(nil)
+    unless meths.empty?
 
-    else
       # get list of methods to remove module
       ims = mod.instance_methods.map {|m| m.to_sym}
       removes = ims - meths
+
       if removes.size != (ims.size - meths.size)
         raise ImportError, "##{(meths - ims).join(' and #')} not found in #{mod}"
       end
 
-      include_module_copy.call( lambda do
+      mod_dup.module_eval do
         removes.each { |meth| remove_method meth }
-      end )
+      end
     end
+
+    include mod_dup
   end
 end
